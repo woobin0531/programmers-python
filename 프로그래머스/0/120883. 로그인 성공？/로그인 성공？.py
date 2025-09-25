e13b57452d03b97ad88eb3af3498bb10396f3dd5
woobin.jeong <cdevfrr@naver.com>
2025-09-25 23:20:08 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,11 @@
+def solution(id_pw, db):
+    user_id, user_pw = id_pw
+    
+    for db_id, db_pw in db:
+        if db_id == user_id:         
+            if db_pw == user_pw:      
+                return "login"
+            else:                     
+                return "wrong pw"
+    
+    return "fail"
